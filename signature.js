0912533eecb0645507333065a930f703f62ae7c5
Siam Rahman <siamr.bro99@gmail.com>
2024-11-20 20:36:35 -0500

    Added operations to change variation number as well as buttons to change the character

--- signature.js
+++ signature.js
@@ -1,6 +1,6 @@
 const $signature = $("#signature").jSignature({
-  width: 70,
-  height: 70,
+  width: 100,
+  height: 100,
   lineWidth: 1,
   UndoButton: false,
 });
@@ -11,19 +11,51 @@ $(document).on("keydown", (e) => {
   }
 });
 
-let variant = 1
+let variant = 1;
+variantElement = $("#variant");
+
+const updateVariantElement = () => {
+  variantElement.text(`Variant: ${variant}`);
+};
+
+let char = 'A'
+
+$(".char-button").on("click", function() {
+  char = $(this).text();
+  variant = 1;
+  updateVariantElement();
+});
+
+$(document).on("keydown", (e) => {
+  if (e.key == "d") {
+    variant -= 1;
+    if (variant < 1) {
+      variant = 1;
+    }
+    updateVariantElement();
+  }
+});
+
+$(document).on("keydown", (e) => {
+  if (e.key == "i") {
+    variant += 1;
+    updateVariantElement();
+  }
+});
 
 $(document).on("keydown", (e) => {
   if (e.key === "s") {
+    console.log("eadf");
     const data = $signature.jSignature("getData", "svgbase64");
     const mimeType = data[0];
     const base64Data = data[1];
     const dataURL = `data:${mimeType},${base64Data}`;
-    
-    const link = document.createElement('a');
+
+    const link = document.createElement("a");
     link.href = dataURL;
-    link.download = `A${variant}.svg`;
+    link.download = `${char}${variant}.svg`;
     variant += 1;
+    updateVariantElement();
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
